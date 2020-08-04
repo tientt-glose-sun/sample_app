b76af00fae572216795488b2ef66f1b0cdf0b3fb
tientt-glose-sun <nguyen.manh.tienb@sun-asterisk.com>
2020-08-04 16:12:54 +0700

    semi-done chap 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:sessions][:email].downcase
     if user&.authenticate params[:sessions][:password]
       log_in user
+      params[:sessions][:remember_me] == Settings.sessions.create.select_box ? remember(user) : forget(user)
       flash[:success] = t ".success_mess"
       redirect_to user_path user
     else
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   USERS_PARAMS = %i(name email password password_confirmation).freeze
 
+  attr_accessor :remember_token
+
   validates :name, presence: true,
     length: {maximum: Settings.validations.name.max_length}
 
@@ -15,6 +17,37 @@ class User < ApplicationRecord
   has_secure_password
 
   before_save :email_downcase
+
+  def remember
+    self.remember_token = User.new_token
+    update :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update :remember_digest, nil
+  end
+
+  class << self
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
 
   private
 
