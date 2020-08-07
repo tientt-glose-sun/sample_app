5946377342494ccdd61a0630abd59f0bb7c76047
tientt-glose-sun <nguyen.manh.tienb@sun-asterisk.com>
2020-08-07 10:56:08 +0700

    semi-done chap 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   USERS_PARAMS = %i(name email password password_confirmation).freeze
+  USERS_PARAMS_RESET = %i(password password_confirmation).freeze
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   scope :is_activated, ->{where activated: true}
 
@@ -44,6 +45,19 @@ class User < ApplicationRecord
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.password_reset.expired_time.hours.ago
   end
 
   class << self
